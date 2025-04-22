7ecccf478b7b26c36cdd1a8b434521545e0cdbbd
EdniMorales <rimora.29@gmail.com>
2025-04-22 13:36:27 -0600

    cambios

--- index.php
+++ index.php
@@ -94,6 +94,10 @@ include './vistas/head.php';
       </div> 
     </div>
   </div>
+
+
+
+
 </section>
 <!-- Portfolio-->
 <section class="content-section" id="portfolio">
@@ -152,6 +156,68 @@ include './vistas/head.php';
       </div>
   </div>
 </section>
+
+
+
+
+<div class="container-fluid ps-md-0">
+  <div class="row g-0">
+    <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+    <div class="col-md-8 col-lg-6">
+      <div class="login d-flex align-items-center py-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-9 col-lg-8 mx-auto">
+              <h3 class="login-heading mb-4">Welcome back!</h3>
+
+              <!-- Sign In Form -->
+              <form>
+                <div class="form-floating mb-3">
+                  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                  <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating mb-3">
+                  <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                  <label for="floatingPassword">Password</label>
+                </div>
+
+                <div class="form-check mb-3">
+                  <input class="form-check-input" type="checkbox" value="" id="rememberPasswordCheck">
+                  <label class="form-check-label" for="rememberPasswordCheck">
+                    Remember password
+                  </label>
+                </div>
+
+                <div class="d-grid">
+                  <button class="btn btn-lg btn-primary btn-login text-uppercase fw-bold mb-2" type="submit">Sign in</button>
+                  <div class="text-center">
+                    <a class="small" href="#">Forgot password?</a>
+                  </div>
+                </div>
+
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <section>
 <div>
@@ -224,14 +290,14 @@ include './vistas/head.php';
                   >
                   <p class="card-text mb-0">
                   Ingredientes:
--Pan Birote
--Queso chipotle
--Mantequilla de La Pilarica
--2 tomates
--1/2 Cebolla blanca
--2 Chile jalapeño
--Sal al gusto
--Frijoles Refritos
+                    -Pan Birote
+                    -Queso chipotle
+                    -Mantequilla de La Pilarica
+                    -2 tomates
+                    -1/2 Cebolla blanca
+                    -2 Chile jalapeño
+                    -Sal al gusto
+                    -Frijoles Refritos
 
                   </p>
 
