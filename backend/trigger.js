f95456ef1342d4e630951c14fd396e3afd3bb0e4
Isaac-Monted <isaacmonted10@gmail.com>
2025-04-25 11:09:49 -0600

    se refactoriza la URL base y agrega lógica de búsqueda de productos

---
+++ backend/trigger.js
@@ -0,0 +1,24 @@
+import * as widgets from './widgets.js';
+
+// Disparadores de los botones del DOM
+export function ProductSearch(Id_Producto, page, dropdown){
+    const Search_Text = document.getElementById(Id_Producto).value;
+    const widget = document.getElementById(dropdown)
+    console.log(`Texto buscado: ${Search_Text}`);
+
+    // Validar que no este vacio
+    if (Search_Text.length > 0){
+        // Ajax asi es servidor
+        fetch(`./php/backend.php?action=searchOnlyProductos&search_prod=${encodeURIComponent(Search_Text)}`)
+            .then(response => response.json()) // Espera la respuesta como JSON
+            .then(data => {
+                console.log("Datos obtenidos: ", data); 
+                widgets.dropdownprod(dropdown , data);
+            })
+            .catch(error => {
+                console.error("Error al buscar productos:", error);
+            });
+    }else{
+        widget.innerHTML = ''
+    }
+};
---
+++ backend/widgets.js
@@ -0,0 +1,25 @@
+
+export function dropdownprod (widget,array) {
+    const ddrop = document.getElementById(widget);
+     ddrop.innerHTML = '';
+ 
+     if(array.error){
+         ddrop.innerHTML = `<p>${array.error}</p>`;
+         return;
+     }
+     
+     if(array.length === 0){
+         ddrop.innerHTML = '<p>No se encontraron productos.</p>';
+         return;
+     }
+ 
+     array.forEach(product => {
+         const drop = document.createElement('div');
+         drop.classList.add('dropdownList');
+         drop.innerHTML= `
+         <h3><a href="articulo.html?id=${product.ID_PRODUCTOS}" target="_blank">${product.NOMBRE}</a></h3>
+     `;
+ 
+     ddrop.appendChild(drop);
+ });
+ }
