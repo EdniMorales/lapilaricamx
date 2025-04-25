1b0f644935ec83a0488a806cb6c24c5bc31882a9
EdniMorales <rimora.29@gmail.com>
2025-04-25 16:34:05 -0600

    cambio

--- config/parameters.php
+++ config/parameters.php
@@ -1,4 +1,4 @@
 <?php
-define("base_url","https://192.168.1.158/paginaWebPilarica/lapilaricamx/"); // base url para llamadas absolutas de la url 
+define("base_url","http://localhost/lapilaricamx/"); // base url para llamadas absolutas de la url 
 define("controller_default", "PrincipalController"); // controlador por defecto
 define("action_default","index");
