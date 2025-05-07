8f7fe45c751140c451c7949f6e72126095791771
EdniMorales <rimora.29@gmail.com>
2025-05-07 08:17:24 -0600

    anexo informacion

--- views/solo/index.php
+++ views/solo/index.php
@@ -36,6 +36,30 @@
                     </div>
                 </div>
             </section>
+                <!-- About section one-->
+                <section class="py-5 bg-light" id="scroll-target">
+                <div class="container px-5 my-5">
+                    <div class="row gx-5 align-items-center">
+                        <div class="col-lg-6"><img class="img-fluid rounded mb-5 mb-lg-0" src="<?= base_url ?>assets/carreras-uni.jpg" alt="..." /></div>
+                        <div class="col-lg-6">
+                            <h2 class="fw-bolder">Degustacion</h2>
+                            <p class="lead fw-normal text-muted mb-0">Nuestro Motor para fabricar productos son nuestros clientes, la razon de ser y crear productos de calidad.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+             <!-- About section two-->
+             <section class="py-5">
+                <div class="container px-5 my-5">
+                    <div class="row gx-5 align-items-center">
+                        <div class="col-lg-6 order-first order-lg-last"><img class="img-fluid rounded mb-5 mb-lg-0" src="<?= base_url ?>assets/carreras-uni.jpg" alt="..." /></div>
+                        <div class="col-lg-6">
+                            <h2 class="fw-bolder">Presentes en los eventos mas importantes</h2>
+                            <p class="lead fw-normal text-muted mb-0">En aperturas continuamos trabajando diariamente para colocar productos frescos, de calidad y artesanales hasta tu mesa</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
             <!-- Team members section-->
          <!--    <section class="py-5 bg-light">
                 <div class="container px-5 my-5">
