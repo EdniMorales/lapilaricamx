335b99a094826cc8c3a55b49d86557c82c01f8d3
Isaac-Monted <isaacmonted10@gmail.com>
2025-05-14 09:42:31 -0600

    refactor: Reorganizar la redirección de controlador y acción en index.php

--- index.php
+++ index.php
@@ -2,9 +2,15 @@
 
 require_once 'autoload.php';
 require_once 'config/parameters.php';
+
+// Redirige si no hay controlador ni acción
+if (!isset($_GET['controller']) && !isset($_GET['action'])) {
+    header('Location: ' . base_url . 'Principal/index');
+    exit();
+}
+
+// se coloca aqui por que si no no redirige
 require_once 'views/layout/head.php';  // cabecera de la pagina
-
-
 
 function show_error(){
     $error = new ErrorController();
@@ -15,14 +21,13 @@ if(isset($_GET['controller'])){
     $nombreControlador = $_GET['controller'].'Controller';
     
 }elseif(!isset($_GET['controller'])  && !isset($_GET['action'])){
-    $nombreControlador = controller_default;  
+    $nombreControlador = controller_default;
 }else{
     show_error();
     exit();
 }
 // comprobar si existe un controlador
-if(class_exists($nombreControlador)){    
-    
+if(class_exists($nombreControlador)){
     $controlador = new $nombreControlador();
     
     if(isset($_GET['action']) && method_exists($controlador, $_GET['action'])){
