6dbbae21f8118278ab8e0eb83ae2d9363277ede0
EdniMorales <rimora.29@gmail.com>
2025-07-15 16:03:35 -0600

    cambios nuevos

--- views/layout/head.php
+++ views/layout/head.php
@@ -53,7 +53,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                              <li class="nav-item">
-                            <a class="nav-link" href="<?= base_url ?>Solo/index">Inicio |</a>
+                            <a class="nav-link" href="<?= base_url ?>Principal/index">Inicio |</a>
                         </li>
                         <li class="nav-item dropdown" id="dmenu">
                             <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button"
